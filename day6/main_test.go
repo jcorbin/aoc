0aa5a8a69b44d94545ddab7158f2671d8147b189
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-08 09:07:04 -0800

    day6 add benchmark

--- day6/main_test.go
+++ day6/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"image"
+	"os"
 	"strings"
 	"testing"
 
@@ -68,4 +69,46 @@ func Test_problem_solve(t *testing.T) {
 	}
 	assert.Equal(t, tc.gridLines, strings.Split(buf.String(), "\n"))
 	assert.Equal(t, tc.counts, prob.countArea())
+}
+
+func Benchmark_problem_populate(b *testing.B) {
+	type benchCase struct {
+		name string
+		in   []image.Point
+	}
+	bcs := []benchCase{
+		{
+			name: "ex",
+			in: []image.Point{
+				image.Pt(1, 1), // #1 A
+				image.Pt(1, 6), // #2 B
+				image.Pt(8, 3), // #3 C
+				image.Pt(3, 4), // #4 D
+				image.Pt(5, 5), // #5 E
+				image.Pt(8, 9), // #6 F
+			},
+		},
+	}
+
+	if f, err := os.Open("input"); err == nil {
+		bc := benchCase{name: f.Name()}
+		bc.in, err = readPoints(f)
+		if cerr := f.Close(); err == nil {
+			err = cerr
+		}
+		if err == nil {
+			bcs = append(bcs, bc)
+		}
+	}
+
+	for _, bc := range bcs {
+		b.Run(bc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				var prob problem
+				prob.points = bc.in
+				prob.init()
+				require.NoError(b, prob.populate())
+			}
+		})
+	}
 }
