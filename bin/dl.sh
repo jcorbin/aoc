d4ff282ad48a44e8c8fe54d9b557ccfae94d15cb
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-07 22:10:59 -0800

    bin/dl: echo url

--- bin/dl.sh
+++ bin/dl.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 url=$(grep -E -o -m1 'https?://adventofcode.com/[0-9]*/day/[0-9]*')
+echo "$url"
+
 curl -s --cookie cookie.txt "$url" \
 | pandoc -f html -t markdown
