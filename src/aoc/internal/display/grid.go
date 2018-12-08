3ec45600e48225e4c49beea6c62024491cda7e33
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-08 15:23:27 -0800

    day6 refactor writeGrid*

--- src/aoc/internal/display/grid.go
+++ src/aoc/internal/display/grid.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/jcorbin/anansi"
+	"github.com/jcorbin/anansi/ansi"
 )
 
 // WriteGrid just writes the grid's contents to the given io.Writer,
@@ -11,38 +12,26 @@ import (
 //
 // TODO haul this back to anansi
 func WriteGrid(w io.Writer, g anansi.Grid) (int64, error) {
-	const border = false
-
 	var buf anansi.Buffer
 	var cur anansi.CursorState
-
-	buf.WriteSGR(cur.MergeSGR(0))
-
-	// header
-	if border {
-		buf.WriteRune('+')
-		for x := g.Rect.Min.X; x < g.Rect.Max.X; x++ {
-			buf.WriteRune('-')
-		}
-		buf.WriteRune('+')
+	cur.Point = ansi.Pt(1, 1)
+	for y := g.Rect.Min.Y; y < g.Rect.Max.Y; y++ {
+		cur = writeGridRow(&buf, cur, g, y)
 		buf.WriteString("\r\n")
+		cur.X = 1
 		cur.Y++
 	}
+	return buf.WriteTo(w)
+}
 
-	if border {
-		buf.WriteRune('|')
-	}
-	for i := 0; i < len(g.Rune); i++ {
-		if i > 0 && i%g.Stride == 0 {
-			buf.WriteSGR(cur.MergeSGR(0))
-			if border {
-				buf.WriteString("|\r\n|")
-			} else {
-				buf.WriteString("\r\n")
-			}
-			cur.X = 1
-			cur.Y++
-		}
+func writeGridRow(
+	buf *anansi.Buffer,
+	cur anansi.CursorState,
+	g anansi.Grid, row int,
+) anansi.CursorState {
+	buf.WriteSGR(cur.MergeSGR(0))
+	for pt := ansi.Pt(g.Rect.Min.X, row); pt.X < g.Rect.Max.X; pt.X++ {
+		i, _ := g.CellOffset(pt)
 		gr, ga := g.Rune[i], g.Attr[i]
 		if gr == 0 {
 			ga = 0
@@ -52,28 +41,6 @@ func WriteGrid(w io.Writer, g anansi.Grid) (int64, error) {
 		buf.WriteRune(gr)
 		cur.X++
 	}
-
-	if cur.X > 1 {
-		buf.WriteSGR(cur.MergeSGR(0))
-		if border {
-			buf.WriteString("|\r\n")
-		} else {
-			buf.WriteString("\r\n")
-		}
-		cur.X = 1
-		cur.Y++
-	}
-
-	// footer
-	if border {
-		buf.WriteRune('+')
-		for x := g.Rect.Min.X; x < g.Rect.Max.X; x++ {
-			buf.WriteRune('-')
-		}
-		buf.WriteRune('+')
-		buf.WriteString("\r\n")
-		cur.Y++
-	}
-
-	return buf.WriteTo(w)
+	buf.WriteSGR(cur.MergeSGR(0))
+	return cur
 }
