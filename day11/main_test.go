492546c23dd70a6aeaf182f80685b4a4e4d9e3d6
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-11 08:29:23 -0800

    day11 add benchmark

--- day11/main_test.go
+++ day11/main_test.go
@@ -25,15 +25,15 @@ func Test_powerLevel(t *testing.T) {
 	}
 }
 
-func Test_solvers(t *testing.T) {
-	solutions := []struct {
-		name    string
-		factory func(serial int, bounds image.Rectangle) solver
-	}{
-		{"fuelGrid", fuelGridSolver},
-		{"spec", specSolver},
-	}
+var testSolutions = []struct {
+	name    string
+	factory func(serial int, bounds image.Rectangle) solver
+}{
+	{"fuelGrid", fuelGridSolver},
+	{"spec", specSolver},
+}
 
+func Test_solvers(t *testing.T) {
 	type result struct {
 		loc   image.Point
 		level int
@@ -52,13 +52,59 @@ func Test_solvers(t *testing.T) {
 
 	bounds := image.Rect(1, 1, 301, 301)
 
-	for _, sol := range solutions {
+	for _, sol := range testSolutions {
 		t.Run(sol.name, func(t *testing.T) {
 			for _, tc := range testCases {
 				t.Run(fmt.Sprintf("serial:%d size:%v", tc.serial, tc.size), func(t *testing.T) {
 					solver := sol.factory(tc.serial, bounds)
 					loc, level := solver.solve(tc.size)
 					assert.Equal(t, tc.result, result{loc, level})
+				})
+			}
+		})
+	}
+}
+
+func Benchmark_solvers(b *testing.B) {
+	benchCases := []struct {
+		serial int
+		size   int
+	}{
+		{18, 3},
+		{18, 16},
+
+		{42, 1},
+		{42, 2},
+		{42, 3},
+		{42, 4},
+		{42, 5},
+		{42, 6},
+		{42, 7},
+		{42, 8},
+		{42, 9},
+		{42, 10},
+		{42, 11},
+		{42, 12},
+		{42, 13},
+		{42, 14},
+		{42, 15},
+		{42, 16},
+		{42, 17},
+		{42, 18},
+		{42, 19},
+		{42, 20},
+	}
+
+	bounds := image.Rect(1, 1, 301, 301)
+
+	for _, sol := range testSolutions {
+		b.Run(sol.name, func(b *testing.B) {
+			for _, bc := range benchCases {
+				b.Run(fmt.Sprintf("serial:%d size:%d", bc.serial, bc.size), func(b *testing.B) {
+					solver := sol.factory(bc.serial, bounds)
+					for i := 0; i < b.N; i++ {
+						_, _ = solver.solve(bc.size)
+					}
 				})
 			}
 		})
