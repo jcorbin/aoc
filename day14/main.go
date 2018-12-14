5bc7e712fabd19cad057ec9612a0d2ffc13e5bbf
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-13 22:43:01 -0800

    day14 micro perf in board.tick

--- day14/main.go
+++ day14/main.go
@@ -102,16 +102,25 @@ func (brd *board) init(e1, e2 int, scores ...uint8) {
 }
 
 func (brd *board) tick() {
-	s1, s2 := brd.scores[brd.e1], brd.scores[brd.e2]
-	tot := s1 + s2
-	d1, d2 := tot/10, tot%10
-	if tot < 10 {
-		brd.scores = append(brd.scores, d2)
+	e1, e2 := brd.e1, brd.e2
+	s1, s2 := brd.scores[e1], brd.scores[e2]
+	if tot := s1 + s2; tot >= 10 {
+		brd.scores = append(brd.scores, tot/10, tot%10)
 	} else {
-		brd.scores = append(brd.scores, d1, d2)
+		brd.scores = append(brd.scores, tot)
 	}
-	brd.e1 = (brd.e1 + 1 + int(s1)) % len(brd.scores)
-	brd.e2 = (brd.e2 + 1 + int(s2)) % len(brd.scores)
+	n := len(brd.scores)
+	e1 = e1 + 1 + int(s1)
+	e2 = e2 + 1 + int(s2)
+
+	for e1 >= n {
+		e1 -= n
+	}
+	for e2 >= n {
+		e2 -= n
+	}
+
+	brd.e1, brd.e2 = e1, e2
 }
 
 func (brd *board) search(pattern []uint8) int {
