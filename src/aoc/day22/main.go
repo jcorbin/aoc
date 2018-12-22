f93d2673a9524703a9879288a6854bccdbe5dd3c
Joshua T Corbin <jcorbin@wunjo.org>
2018-12-22 02:26:21 -0800

    day22 simplify and fix part2

--- src/aoc/day22/main.go
+++ src/aoc/day22/main.go
@@ -126,26 +126,32 @@ func (sc *scan) erosionLevel(p image.Point) int {
 type regionType uint8
 
 const (
-	regionInvalid regionType = iota
-	regionRocky
+	regionRocky regionType = iota
 	regionWet
 	regionNarrow
 )
 
+type equipType uint8
+
+const (
+	// In rocky regions, you can use the climbing gear or the torch.
+	// You cannot use neither (you'll likely slip and fall).
+	equipNothing equipType = iota // NOTE == regionRocky
+
+	// In wet regions, you can use the climbing gear or neither tool.
+	// You cannot use the torch (if it gets wet, you won't have a light source).
+	equipTorch // NOTE == regionWet
+
+	// In narrow regions, you can use the torch or neither tool.
+	// You cannot use the climbing gear (it's too bulky to fit).
+	equipGear // NOTE == regionNarrow
+)
+
 func (sc *scan) regionType(p image.Point) regionType {
-	el := sc.erosionLevel(p)
-	switch el % 3 {
 	// If the erosion level modulo 3 is 0, the region's type is rocky.
-	case 0:
-		return regionRocky
 	// If the erosion level modulo 3 is 1, the region's type is wet.
-	case 1:
-		return regionWet
 	// If the erosion level modulo 3 is 2, the region's type is narrow.
-	case 2:
-		return regionNarrow
-	}
-	return regionInvalid
+	return regionType(sc.erosionLevel(p) % 3)
 }
 
 var (
@@ -156,171 +162,80 @@ var (
 
 type explorerState struct {
 	image.Point
-	torch bool
-	gear  bool
+	equip equipType
 }
 
 type explorer struct {
-	time   int
-	id     int
-	parent int
 	explorerState
+	time int
 }
 
 func (ex explorer) String() string {
-	return fmt.Sprintf("t:%v @%v torch:%v gear:%v", ex.time, ex.Point, ex.torch, ex.gear)
+	return fmt.Sprintf("t:%v @%v equip:%v", ex.time, ex.Point, ex.equip)
 }
 
 func (ex explorer) done(sc *scan) bool {
-	return ex.Point == sc.target && ex.torch
+	return ex.Point == sc.target && ex.equip == equipTorch
 }
 
-func (ex explorer) hasBeen(p image.Point, s *search) bool {
-	if p == image.ZP {
-		return true
-	}
-	for id := ex.id; id != 0; id = s.par[id] {
-		if s.been[id] == p {
-			return true
-		}
-	}
-	return false
-}
-
-func (ex explorer) expand(s *search) explorer {
+func (ex explorer) expand(s *search) {
 	// Finally, once you reach the target, you need the *torch* equipped before
 	// you can find him in the dark. The target is always in a *rocky* region,
 	// so if you arrive there with *climbing gear* equipped, you will need to
 	// spend seven minutes switching to your torch.
 	if ex.Point == s.target {
-		if !ex.torch {
-			ex.torch, ex.gear = true, false
+		if ex.equip != equipTorch {
+			ex.equip = equipTorch
 			ex.time += 7
+			s.add(ex)
 		}
-		return ex
+		return
 	}
 
-	var retEx explorer
+	// You can change your currently equipped tool or put both away if your new
+	// equipment would be valid for your current region. Switching to using the
+	// climbing gear, torch, or neither always takes seven minutes, regardless
+	// of which tools you start with. (For example, if you are in a rocky
+	// region, you can switch from the torch to the climbing gear, but you
+	// cannot switch to neither.)
+	reg := s.regionType(ex.Point)
+	for _, eq := range [...]equipType{equipNothing, equipTorch, equipGear} {
+		if eq != ex.equip && eq != equipType(reg) {
+			next := ex
+			next.time += 7
+			next.equip = eq
+			s.add(next)
+		}
+	}
+
+	// You can move to an adjacent region (up, down, left, or right; never
+	// diagonally) if your currently equipped tool allows you to enter that
+	// region. Moving to an adjacent region takes one minute. (For example,
+	// if you have the torch equipped, you can move between rocky and
+	// narrow regions, but cannot enter wet regions.)
 	for _, dp := range [...]image.Point{
-		image.Pt(0, -1),
-		image.Pt(1, 0),
-		image.Pt(0, 1),
-		image.Pt(-1, 0),
+		image.Pt(0, -1), image.Pt(0, 1),
+		image.Pt(1, 0), image.Pt(-1, 0),
 	} {
 		p := ex.Point.Add(dp)
 		if p.X < 0 || p.Y < 0 {
 			continue
 		}
-		if ex.hasBeen(p, s) {
+		if ex.equip == equipType(s.regionType(p)) {
 			continue
-		}
-
-		// You can move to an adjacent region (up, down, left, or right; never
-		// diagonally) if your currently equipped tool allows you to enter that
-		// region. Moving to an adjacent region takes one minute. (For example,
-		// if you have the torch equipped, you can move between rocky and
-		// narrow regions, but cannot enter wet regions.)
-
-		// You can change your currently equipped tool or put both away if your
-		// new equipment would be valid for your current region. Switching to
-		// using the climbing gear, torch, or neither always takes seven
-		// minutes, regardless of which tools you start with. (For example, if
-		// you are in a rocky region, you can switch from the torch to the
-		// climbing gear, but you cannot switch to neither.)
-
-		next := ex.move(s, p)
-		if next.time == 0 || next == ex {
-			continue
-		}
-
-		if retEx.time == 0 {
-			retEx = next
-		} else if s.preferExplorer(next, retEx) {
-			s.add(retEx)
-			retEx = next
-		} else {
-			s.add(next)
-		}
-
-	}
-	return retEx
-}
-
-func (ex explorer) move(s *search, p image.Point) explorer {
-	switch s.regionType(p) {
-	// In rocky regions, you can use the climbing gear or the torch.  You
-	// cannot use neither (you'll likely slip and fall).
-	case regionRocky:
-		if ex.torch || ex.gear {
-			ex.time++
-			ex.Point = p
-			return ex
 		}
 
 		next := ex
 		next.time++
 		next.Point = p
-		next.time += 7
-		next.torch, next.gear = true, false
 		s.add(next)
-
-		next.torch, next.gear = false, true
-		s.add(next)
-
-		next.torch, next.gear = true, true
-		return next
-
-	// In wet regions, you can use the climbing gear or neither tool.  You
-	// cannot use the torch (if it gets wet, you won't have a light source).
-	case regionWet:
-		ex.time++
-		ex.Point = p
-		if ex.torch {
-			ex.time += 7
-			ex.torch = false
-			if ex.gear {
-				s.add(ex)
-				ex.gear = false
-			}
-		} else if ex.gear {
-			next := ex
-			next.time += 7
-			next.gear = false
-			s.add(next)
-		}
-		return ex
-
-	// In narrow regions, you can use the torch or neither tool. You cannot use
-	// the climbing gear (it's too bulky to fit).
-	case regionNarrow:
-		ex.time++
-		ex.Point = p
-		if ex.gear {
-			ex.time += 7
-			ex.gear = false
-			if ex.torch {
-				s.add(ex)
-				ex.torch = false
-			}
-		} else if ex.torch {
-			next := ex
-			next.time += 7
-			next.torch = false
-			s.add(next)
-		}
-		return ex
-
 	}
-	return explorer{}
 }
 
 type search struct {
 	*scan
-	been     []image.Point
-	par      []int
 	frontier []explorer
 	seen     map[explorerState]int
-	pend     map[explorerState]int
 	best     explorer
 	risk     map[image.Point]int
 }
@@ -379,51 +294,28 @@ func signPoint(p image.Point) image.Point {
 }
 
 func (s *search) preferExplorer(a, b explorer) bool {
-	da := manhattanDistance(a.Point, s.target)
-	db := manhattanDistance(b.Point, s.target)
-
-	da *= s.riskAt(a.Point)
-	db *= s.riskAt(b.Point)
-
-	// return da < db
-
-	if da < db {
+	if a.time < b.time {
 		return true
 	}
-	if db < da {
+	if a.time > b.time {
 		return false
 	}
-	return a.time < b.time
-
-	// if a.time < b.time {
-	// 	return true
-	// }
-	// if a.time > b.time {
-	// 	return false
-	// }
-
-	// td := s.target.X + s.target.Y
-	// if da < 0 {
-	// 	da *= da
-	// }
-	// if db < 0 {
-	// 	db *= db
-	// }
-	// return da < db
-
+	if a.X < b.X {
+		return true
+	}
+	if a.X > b.X {
+		return false
+	}
+	if a.Y < b.Y {
+		return true
+	}
+	if a.Y > b.Y {
+		return false
+	}
+	return a.equip < b.equip
 }
 
 func (s *search) add(next explorer) {
-	if s.best.time > 0 && next.time > s.best.time {
-		return
-	}
-	if t, def := s.seen[next.explorerState]; def && t < next.time {
-		return
-	}
-	if t, def := s.pend[next.explorerState]; def && t < next.time {
-		return
-	}
-	s.pend[next.explorerState] = next.time
 	heap.Push(s, next)
 }
 
@@ -441,10 +333,6 @@ func manhattanDistance(a, b image.Point) int {
 
 func (s *search) Push(x interface{}) {
 	ex := x.(explorer)
-	ex.parent = ex.id
-	ex.id = len(s.been)
-	s.been = append(s.been, ex.Point)
-	s.par = append(s.par, ex.parent)
 	s.frontier = append(s.frontier, ex)
 }
 
@@ -452,33 +340,6 @@ func (s *search) Pop() interface{} {
 	x := s.frontier[len(s.frontier)-1]
 	s.frontier = s.frontier[:len(s.frontier)-1]
 	return x
-}
-
-func (s *search) cull() (n int) {
-	i := 0
-	for j := 0; j < len(s.frontier); j++ {
-		ex := s.frontier[j]
-
-		if s.best.time > 0 && ex.time > s.best.time {
-			n++
-			continue
-		}
-		if t, def := s.seen[ex.explorerState]; def && t < ex.time {
-			n++
-			continue
-		}
-
-		if i != j {
-			s.frontier[i] = s.frontier[j]
-		}
-		i++
-
-	}
-	if i < len(s.frontier) {
-		s.frontier = s.frontier[:i]
-		heap.Init(s)
-	}
-	return n
 }
 
 func run(in, out *os.File) error {
@@ -490,36 +351,18 @@ func run(in, out *os.File) error {
 	max := sc.target.Add(image.Pt(1, 1))
 
 	// part 1
-	// var buf bytes.Buffer
 	risk := 0
 	for p := min; p.Y < max.Y; p.Y++ {
 		for p.X = min.X; p.X < max.X; p.X++ {
-			rt := sc.regionType(p)
-			switch rt {
-			// case regionRocky:
-			// 	buf.WriteByte('.')
-			case regionWet:
-				// buf.WriteByte('=')
-				risk++
-			case regionNarrow:
-				// buf.WriteByte('|')
-				risk += 2
-			default:
-				// buf.WriteByte('?')
-			}
+			risk += int(sc.regionType(p))
 		}
-		// buf.WriteByte('\n')
 	}
-	// buf.WriteTo(out)
 	log.Printf("total risk: %v", risk)
 
 	// part 2
 	var s search
 	s.scan = &sc
-	s.been = make([]image.Point, 0, 1024*1024)
-	s.par = make([]int, 0, 1024*1024)
 	s.seen = make(map[explorerState]int, 1024*1024)
-	s.pend = make(map[explorerState]int, 1024*1024)
 
 	tick := time.NewTicker(time.Second)
 	n := 0
@@ -528,21 +371,14 @@ func run(in, out *os.File) error {
 	heap.Push(&s, explorer{
 		explorerState: explorerState{
 			Point: image.ZP,
-			torch: true,
-			gear:  false,
+			equip: equipTorch,
 		},
 	})
 
-	for {
-		if s.Len() == 0 {
-			break
-		}
-
+	for s.Len() > 0 {
 		ex := heap.Pop(&s).(explorer)
-		delete(s.pend, ex.explorerState)
 
-	deepen:
-		if t, def := s.seen[ex.explorerState]; def && t < ex.time {
+		if t, def := s.seen[ex.explorerState]; def && t <= ex.time {
 			continue
 		}
 		s.seen[ex.explorerState] = ex.time
@@ -553,31 +389,26 @@ func run(in, out *os.File) error {
 		select {
 		case now := <-tick.C:
 			log.Printf(
-				"search % -40v (w/ % 8v rem, % 8v con, %.1f/s) best:%v",
+				"search % -25v (w/ % 8v rem, % 8v con, %.1f/s) best:%v",
 				ex, s.Len(), n, float64(n)/(float64(now.Sub(t0))/float64(time.Second)),
 				s.best.time,
 			)
-			if n := s.cull(); n > 0 {
-				log.Printf("opp culled %v", n)
-			}
 		default:
 		}
 
 		n++
-		if ex.done(&sc) {
-			if s.best.time == 0 || ex.time < s.best.time {
-				s.best = ex
-				n := s.cull()
-				log.Printf("FOUND %v culled:%v", ex, n)
-			}
-		} else if ex = ex.expand(&s); ex.time != 0 {
-			if ex.time >= 200 {
-				goto deepen
-			}
-			s.add(ex)
+		if !ex.done(&sc) {
+			ex.expand(&s)
+		} else if s.best.time == 0 || ex.time < s.best.time {
+			s.best = ex
+			log.Printf("FOUND %v", ex)
 		}
-
 	}
+
+	log.Printf(
+		"searched % -25v (% 8v con, %.1f/s)",
+		s.best, n, float64(n)/(float64(time.Now().Sub(t0))/float64(time.Second)),
+	)
 
 	return nil
 }
