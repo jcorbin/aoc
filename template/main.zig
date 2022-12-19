975b5e67fae2c0496d9c97b56f1e6dcbe3522323
Joshua T Corbin <jcorbin@wunjo.org>
2022-12-19 16:20:26 -0500

    template: update main/args/perf from day16

--- template/main.zig
+++ template/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const mem = std.mem;
+
 const Allocator = mem.Allocator;
 
 test "example" {
@@ -21,7 +22,7 @@ test "example" {
                 .verbose = 1,
             },
             .input = example_input,
-            .expected = 
+            .expected =
             \\# Solution
             \\> 42
             \\
@@ -67,13 +68,29 @@ const Config = struct {
 
 const Builder = struct {
     allocator: Allocator,
+    arena: std.heap.ArenaAllocator,
     // TODO state to be built up line-to-line
 
     const Self = @This();
 
+    pub fn parse(allocator: Allocator, reader: anytype) !World {
+        var lines = Parse.lineScanner(reader);
+        var builder = init: {
+            var cur = try lines.next() orelse return error.NoInput;
+            break :init Builder.initLine(allocator, cur) catch |err| return cur.carp(err);
+        };
+        // var lineTime = try timing.timer(.parseLine);
+        while (try lines.next()) |cur| {
+            builder.parseLine(cur) catch |err| return cur.carp(err);
+            // TODO bring back: try lineTime.lap();
+        }
+        return builder.finish();
+    }
+
     pub fn initLine(allocator: Allocator, cur: *Parse.Cursor) !Self {
         var self = Self{
             .allocator = allocator,
+            .arena = std.heap.ArenaAllocator.init(allocator),
         };
         try self.parseLine(cur);
         return self;
@@ -85,20 +102,25 @@ const Builder = struct {
     }
 
     pub fn finish(self: *Self) World {
-        _ = self;
+        // XXX(if may fail): errdefer self.arena.deinit();
+
         return World{
+            .allocator = self.allocator,
+            .arena = self.arena,
             // TODO finalized problem data
         };
     }
 };
 
 const World = struct {
+    allocator: Allocator,
+    arena: std.heap.ArenaAllocator,
     // TODO problem representation
 
     const Self = @This();
 
     pub fn deinit(self: *Self) void {
-        _ = self; // TODO free built data
+        self.arena.deinit();
     }
 };
 
@@ -122,24 +144,11 @@ fn run(
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
 
-    var world = build: { // FIXME: parse input (store intermediate form, or evaluate)
-        var lines = Parse.lineScanner(input.reader());
-        var builder = init: {
-            var cur = try lines.next() orelse return error.NoInput;
-            break :init Builder.initLine(arena.allocator(), cur) catch |err| return cur.carp(err);
-        };
-        var lineTime = try timing.timer(.parseLine);
-        while (try lines.next()) |cur| {
-            builder.parseLine(cur) catch |err| return cur.carp(err);
-            try lineTime.lap();
-        }
-        break :build builder.finish();
-    };
+    var world = try Builder.parse(allocator, input.reader());
     defer world.deinit();
     try timing.markPhase(.parse);
 
     // FIXME: solve...
-    _ = world;
     try timing.markPhase(.solve);
 
     try out.print(
@@ -157,12 +166,25 @@ fn run(
 const ArgParser = @import("args.zig").Parser;
 
 const MainAllocator = std.heap.GeneralPurposeAllocator(.{
-    // .verbose_log = true,
+    .enable_memory_limit = true,
+    .verbose_log = false,
 });
 
+var gpa = MainAllocator{};
+
+var log_level: std.log.Level = .warn;
+
+pub fn log(comptime level: std.log.Level, comptime scope: @TypeOf(.EnumLiteral), comptime format: []const u8, args: anytype) void {
+    if (@enumToInt(level) > @enumToInt(log_level)) return;
+    const prefix = "[" ++ comptime level.asText() ++ "] (" ++ @tagName(scope) ++ "): ";
+    std.debug.getStderrMutex().lock();
+    defer std.debug.getStderrMutex().unlock();
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print(prefix ++ format ++ "\n", args) catch return;
+}
+
 pub fn main() !void {
-    var gpa = MainAllocator{};
-    defer _ = gpa.deinit();
+    gpa.setRequestedMemoryLimit(4 * 1024 * 1024 * 1024);
 
     var allocator = gpa.allocator();
 
@@ -178,12 +200,15 @@ pub fn main() !void {
         var args = try ArgParser.init(argsArena.allocator());
         defer args.deinit();
 
-        // TODO: input filename arg
-
         while (args.next()) |arg| {
-            if (arg.is(.{ "-h", "--help" })) {
+            if (!arg.isOption()) {
+                var prior = input;
+                input = try std.fs.cwd().openFile(arg.have, .{});
+                prior.close();
+                std.log.info("reading input from {s}", .{arg.have});
+            } else if (arg.is(.{ "-h", "--help" })) {
                 std.debug.print(
-                    \\Usage: {s} [-v]
+                    \\Usage: {s} [options] [INPUT_FILE]
                     \\
                     \\Options:
                     \\
@@ -197,6 +222,8 @@ pub fn main() !void {
                 , .{args.progName()});
                 std.process.exit(0);
             } else if (arg.is(.{ "-v", "--verbose" })) {
+                if (@enumToInt(log_level) < @enumToInt(std.log.Level.debug))
+                    log_level = @intToEnum(std.log.Level, @enumToInt(log_level) + 1);
                 config.verbose += 1;
             } else if (arg.is(.{"--raw-output"})) {
                 bufferOutput = false;
@@ -204,14 +231,24 @@ pub fn main() !void {
         }
     }
 
-    var bufin = std.io.bufferedReader(input.reader());
+    (do_run: {
+        var bufin = std.io.bufferedReader(input.reader());
 
-    if (!bufferOutput)
-        return run(allocator, &bufin, output, config);
+        if (!bufferOutput)
+            break :do_run run(allocator, &bufin, output, config);
 
-    var bufout = std.io.bufferedWriter(output.writer());
-    try run(allocator, &bufin, &bufout, config);
-    try bufout.flush();
-    // TODO: sentinel-buffered output writer to flush lines progressively
-    // ... may obviate the desire for raw / non-buffered output else
+        var bufout = std.io.bufferedWriter(output.writer());
+        defer bufout.flush() catch {};
+
+        break :do_run run(allocator, &bufin, &bufout, config);
+        // TODO: sentinel-buffered output writer to flush lines progressively
+        // ... may obviate the desire for raw / non-buffered output else
+    }) catch |err| {
+        if (err == mem.Allocator.Error.OutOfMemory) {
+            const have_leaks = gpa.detectLeaks();
+            std.log.err("{} detectLeaks() -> {}", .{ err, have_leaks });
+        }
+        return err;
+    };
+    if (gpa.detectLeaks()) return error.LeakedMemory;
 }
